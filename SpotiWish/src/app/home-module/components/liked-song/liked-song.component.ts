8634db94bb287ae7f73f03e9177ad65547b7a989
Schmoul622 <samuel.brisset@protonmail.com>
2021-05-17 10:20:12 +0200

    creaate page liked song

---
+++ SpotiWish/src/app/home-module/components/liked-song/liked-song.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liked-song',
+  templateUrl: './liked-song.component.html',
+  styleUrls: ['./liked-song.component.css']
+})
+export class LikedSongComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
